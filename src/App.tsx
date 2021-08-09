8082ab8acf853662ede97080d2d97b9b8f5bb421
JiaLin <linao264590000@gmail.com>
2021-08-09 19:45:54 +0800

    components/PreviewPoll: render completed pewview poll

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import "./App.css";
 import { OngoinPoll } from "./components/OngoingRoll";
 import { PreviewRoll } from "./components/PreviewRoll";
 import data from "./database.json";
+
 function App() {
   const { polls } = data;
   const [selectedPoll, setSelectedPoll] = useState(polls[0]);
   return (
     <div className="App">
       <OngoinPoll poll={selectedPoll} />
-      {polls.map((poll, index) => (
-        <PreviewRoll
-          key={index}
-          poll={poll}
-          toOngoingPollFn={setSelectedPoll}
-        />
-      ))}
+      <div id="preview-root">
+        {polls.map((poll, index) => (
+          <PreviewRoll
+            key={index}
+            poll={poll}
+            toOngoingPollFn={setSelectedPoll}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/PreviewRoll.tsx
+++ src/components/PreviewRoll.tsx
@@ -1,4 +1,5 @@
 import React, { memo } from "react";
+import { PieChart, Pie, Cell } from "recharts";
 import { Poll } from "../PollType";
 
 type PreviewPollProps = {
@@ -8,9 +9,38 @@ type PreviewPollProps = {
 
 export const PreviewRoll = memo<PreviewPollProps>(function PreviewRoll(props) {
   const { poll, toOngoingPollFn } = props;
+  // just for preview, use this mock data
+  const mockData = [
+    { name: "yes", value: 50 },
+    { name: "no", value: 50 },
+  ];
   return (
-    <div id="preview-root" onClick={() => toOngoingPollFn(poll)}>
-      {poll.title}
+    <div className="preview-item" onClick={() => toOngoingPollFn(poll)}>
+      <div className="preview-item-chart">
+        <PieChart width={160} height={160}>
+          <Pie
+            data={mockData}
+            cx={"50%"}
+            cy={"50%"}
+            innerRadius={20}
+            outerRadius={60}
+            paddingAngle={1}
+            dataKey="value"
+          >
+            {mockData.map((_: any, pollInnerIndex: number) => (
+              <Cell
+                key={`cell-${pollInnerIndex}`}
+                fill={pollInnerIndex % 2 === 0 ? "#da621f" : "#79aac5"}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+        <p>%</p>
+      </div>
+      <div className="preview-item-description">
+        <h5>{poll.time}</h5>
+        <p>{poll.title}</p>
+      </div>
     </div>
   );
 });
